f08b9cc5efbfa26ec46dcede61b0e7ec964c5b5f
Divya Borade <boradedivya2005@gmail.com>
2026-05-23 17:02:20 +0530

    Add first hello API endpoint

---
+++ src/main/java/taskflow/controller/HelloController.java
@@ -0,0 +1,13 @@
+package taskflow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Divya, Spring Boot is working!";
+    }
+}
